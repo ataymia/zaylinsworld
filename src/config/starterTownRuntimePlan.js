707ddaafdaff3a42fa9316909c067ee52319d363
ataymia <ataymia.murray@allstarservicesnow.com>
2026-07-18 13:53:37 -0700

    Connect Starter Town road network across every district

--- src/config/starterTownRuntimePlan.js
+++ src/config/starterTownRuntimePlan.js
@@ -89,10 +89,10 @@ export const STARTER_TOWN_ROUTES = freeze([
     [-860, -860], [860, -860], [860, 860], [-860, 860], [-860, -860],
   ], { closed: true, lanes: 4 }),
   route('dreamdrop-boulevard', 'Dreamdrop Boulevard', 'main', [
-    [-900, -120], [-500, -120], [0, -120], [500, -120], [900, -120],
+    [-900, -120], [-500, -120], [0, -120], [300, -120], [500, -120], [900, -120],
   ]),
   route('centre-avenue', 'Centre Avenue', 'main', [
-    [0, -900], [0, -500], [0, 0], [0, 500], [0, 900],
+    [0, -900], [0, -700], [0, -500], [0, -120], [0, 0], [0, 40], [0, 500], [0, 700], [0, 900],
   ]),
   route('northworks-expressway', 'Northworks Expressway', 'expressway', [
     [-900, -700], [-450, -700], [0, -700], [500, -700], [900, -700],
@@ -101,19 +101,19 @@ export const STARTER_TOWN_ROUTES = freeze([
     [0, -860], [0, -1000], [0, -1200],
   ], { gatewayId: 'st-north-hwy' }),
   route('eastgate-parkway', 'Eastgate Parkway', 'parkway', [
-    [360, 40], [650, 20], [860, 0], [1000, 0], [1200, 0],
+    [0, 40], [360, 40], [650, 20], [860, 0], [1000, 0], [1200, 0],
   ], { gatewayId: 'st-east-parkway' }),
   route('civic-rise', 'Civic Rise', 'main', [
-    [300, -520, 4], [430, -420, 10], [560, -300, 18], [720, -180, 26],
+    [300, -120, 0], [360, -220, 4], [430, -320, 10], [560, -300, 18], [720, -180, 26],
   ], { graded: true }),
   route('scholar-road', 'Scholar Road', 'main', [
-    [-720, -340], [-720, 20], [-720, 240],
+    [-720, -340], [-720, -120], [-720, 0], [-720, 240],
   ]),
   route('school-loop', 'School Loop', 'local', [
-    [-810, 0], [-640, 0], [-620, 150], [-800, 150], [-810, 0],
+    [-810, 0], [-720, 0], [-640, 0], [-620, 150], [-800, 150], [-810, 0],
   ], { closed: true, schoolZone: true }),
   route('parkside-crescent', 'Parkside Crescent', 'local', [
-    [250, 500], [340, 430], [470, 410], [590, 470], [650, 580], [610, 710], [500, 780],
+    [0, 500], [250, 500], [340, 430], [470, 410], [590, 470], [650, 580], [610, 710], [500, 780],
   ], { roundaboutAt: point(420, 568) }),
   route('willowbend-main', 'Willowbend Drive', 'local', [
     [-300, 700], [0, 700], [180, 760], [180, 930],
@@ -122,13 +122,13 @@ export const STARTER_TOWN_ROUTES = freeze([
     [-180, 700], [-180, 880], [-235, 925],
   ], { culDeSac: true }),
   route('willowbend-court-b', 'Dreamleaf Court', 'local', [
-    [60, 710], [60, 900], [115, 945],
+    [60, 720], [60, 900], [115, 945],
   ], { culDeSac: true }),
   route('market-service-road', 'Market Service Road', 'service', [
-    [-330, 500], [-80, 500], [200, 500],
+    [-420, 360], [-330, 500], [-80, 500], [0, 500], [200, 500],
   ]),
   route('civic-service-road', 'Civic Service Road', 'service', [
-    [450, -420], [650, -420], [790, -350],
+    [430, -320], [650, -420], [790, -350],
   ]),
   route('westside-alley', 'Westside Back Lane', 'alley', [
     [-900, 360], [-650, 360], [-420, 360],
@@ -164,7 +164,7 @@ export const STARTER_TOWN_SPAWNS = freeze([
 
 export const STARTER_TOWN_RUNTIME_PLAN = freeze({
   id: 'starter-town',
-  version: 1,
+  version: 2,
   origin: point(0, 0),
   playableBounds: freeze({ minX: -1000, maxX: 1000, minZ: -1000, maxZ: 1000 }),
   terrainBounds: freeze({ minX: -1200, maxX: 1200, minZ: -1200, maxZ: 1200 }),
