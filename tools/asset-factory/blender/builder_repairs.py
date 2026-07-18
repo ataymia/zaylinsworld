4ed8a347f1fcdf29b22850ada63ca5750bdf8081
ataymia <ataymia.murray@allstarservicesnow.com>
2026-07-18 11:55:00 -0700

    Register visually open accessibility gate builder

--- tools/asset-factory/blender/builder_repairs.py
+++ tools/asset-factory/blender/builder_repairs.py
@@ -25,11 +25,11 @@ from builder_repairs_contracts import (
 from builder_repairs_food import build_modular_food_fixed
 from builder_repairs_screen import build_wall_screen_fixed
 from builder_repairs_priority import (
-    build_accessibility_gate,
     build_airlock_conduit,
     build_bubble_lift,
     build_elevated_road_support,
 )
+from builder_repairs_accessibility import build_accessibility_gate_open
 
 
 def install_repairs(builders):
@@ -49,7 +49,7 @@ def install_repairs(builders):
         "bubble_lift": build_bubble_lift,
         "airlock_conduit": build_airlock_conduit,
         "elevated_road_support": build_elevated_road_support,
-        "accessibility_gate": build_accessibility_gate,
+        "accessibility_gate": build_accessibility_gate_open,
         "modular_connector": build_modular_connector,
         "modular_infrastructure": build_modular_infrastructure,
         "modular_furniture": build_modular_furniture,
